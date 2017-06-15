9701f8acd8ebd0b46e77c1fe6c26b158a99baa58
Tobias Kopelke <kopelke@gmail.com>
2017-06-15 11:28:05 +0200

    moved typings into lib file

--- lib/log.ts
+++ lib/log.ts
@@ -1,7 +1,43 @@
+export namespace nxLogger {
 
-import {
-  nxLogger
-} from '../types'
+  // define atomic types
+  export type Result = void
+  export type Namespace = string[]
+  export type MessageString = string
+  export type MessageObject = string
+  export type Message = MessageString | MessageObject
+
+  // define config type
+  export interface ConfigPartial {
+    enabled?: boolean
+    namespace?: Namespace
+    transport?: TransportFn
+    tty?: boolean
+  }
+
+  export interface Config extends ConfigPartial {
+    enabled: boolean
+    namespace: Namespace
+    transport: TransportFn
+    tty: boolean
+  }
+
+  // define transport
+  export type TransportFn = (configuration: Config, messages: Message[]) => Result
+
+  // define log function
+  export interface Log {
+    (message: Message): Result
+    readonly configuration: Config
+    create?: FactoryFn
+  }
+  export type FactoryFn = (configuration: ConfigPartial) => Log
+  export type FactoryCreatorFn = (configuration: Config) => FactoryFn
+  export type SimplyFactoryFn = (configuration: Config | Namespace) => Log
+  export type WriteFn = (configuration: Config) => (...messages: Message[]) => Result
+  export type ConfigureFn = (options?: ConfigPartial) => Config
+
+}
 
 // Global LogConfig
 const baseConfiguration: nxLogger.Config = {
