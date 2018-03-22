cb37bc5f56a478e53fd3b12f855f97d35c640c77
Tobias Kopelke <tobias.kopelke@liquidlabs.de>
2018-03-22 09:29:49 +0100

    chore(config): config files updated

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,27 @@
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: 'node',
   transform: {
-    ".(ts|tsx)": "<rootDir>/node_modules/ts-jest/preprocessor.js"
+    '.(ts|tsx)': '<rootDir>/node_modules/ts-jest/preprocessor.js'
   },
   clearMocks: true,
   bail: true,
   modulePaths: [
-    "src",
-    "node_modules"
+    'src',
+    'node_modules'
   ],
   moduleDirectories: [
-    "node_modules",
-    "<rootDir>/src"
+    'node_modules',
+    '<rootDir>/src'
   ],
   moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx"
+    'ts',
+    'tsx',
+    'js',
+    'jsx'
   ],
-  // setupFiles: [ "<rootDir>/mocks/index.ts" ],
-  testRegex: "/lib/.*\.spec\.[tj]sx?$",
-  coverageDirectory: "coverage",
+  testRegex: '/lib/.*\.spec\.[tj]sx?$',
+  coverageDirectory: 'coverage',
   collectCoverageFrom: [
-    "lib/**/*.{ts,tsx,js,jsx}"
+    'lib/**/*.{ts,tsx,js,jsx}'
   ],
 };
